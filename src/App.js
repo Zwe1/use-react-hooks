67415aaf027627cd02224c3fbb42e38c6edb8df0
Zwe1 <771776107@qq.com>
2018-12-11 00:56:14 +0800

    版本回退，更新PDF

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import { Router, Route } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
 import UseState from './main/hookComponent';
 import UseEffect from './main/useEffect';
 import Muso from './main/usestate';
-import Sider from './main/sider';
 let visible = true;
 
 class App extends Component {
@@ -16,16 +14,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <div className='content'>
-          {/* <Sider /> */}
-          <Router>
-            <Route path="/" exact component={UseState}>
-              <Route path="/useState" component={UseState} />
-              <Route path="/useEffect" component={UseEffect} />
-              <Route path="/customizeHooks" component={UseEffect} />
-            </Route>
-          </Router>
-          {/* {visible && <UseState visible={visible}/>}
-          <UseEffect /> */}
+          {visible && <UseState visible={visible}/>}
+          <UseEffect />
         </div>
       </div>
     );
